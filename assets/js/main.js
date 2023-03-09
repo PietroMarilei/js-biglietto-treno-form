597b37ca3c4b999b530daea2a879606eb47c187b
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-09 15:40:55 +0100

    add working calculator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,40 @@
-let travelLenght
-let userAge
+
 
 
 document.getElementById('submit').addEventListener('click',
 function () {
 
-    let travelLenght = document.getElementById('travelLenght').value;
+    const travelLenght = document.getElementById('travelLenght').value;
     console.log (travelLenght);
 
-    let userAge = document.getElementById('userAge').value;
+    const userAge = document.getElementById('userAge').value;
     console.log (userAge);
+// ho i due valori dell'utente
+    const ticketPrice = (travelLenght * 0.21);
+
+    console.log (ticketPrice);
+// ho il prezzo base
+   if (userAge <= 18) {
+   const youngPrice = (ticketPrice - (ticketPrice / 100 * 20)).toFixed(2) ;
+
+
+   document.getElementById("answear").innerHTML = `Il tuo prezzo giovani é ${youngPrice}€`;
+
+   console.log(youngPrice);
+
+}else if (userAge >= 65){
+    const elderPrice = (ticketPrice - (ticketPrice /100 * 40)).toFixed(2);
+
+
+    document.getElementById("answear").innerHTML = `Il tuo prezzo anziani é ${elderPrice}€`;
+
+    console.log(elderPrice);
+}
+
+else {
+    document.getElementById("answear").innerHTML = `Il tuo prezzo normale é ${ticketPrice}€`;
+}
+
 
 
 

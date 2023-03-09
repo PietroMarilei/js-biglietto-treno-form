f09690603e2d36fa968c13ec609726018cbdd6d0
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-09 15:31:53 +0100

    add event listener working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,22 @@
-const travelLenght = document.getElementById(travelLenght).value;
+let travelLenght
+let userAge
+
+
+document.getElementById('submit').addEventListener('click',
+function () {
+
+    let travelLenght = document.getElementById('travelLenght').value;
+    console.log (travelLenght);
+
+    let userAge = document.getElementById('userAge').value;
+    console.log (userAge);
+
+
+
+
+}
+)
+
+
+
 

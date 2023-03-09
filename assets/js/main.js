98415f712d1d7a45ee7f28740811237606f8816e
PietroMarilei~ <pietromrl0@gmail.com>
2023-03-09 17:43:25 +0100

    add some style to the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,37 +4,41 @@
 document.getElementById('submit').addEventListener('click',
 function () {
 
+    const userData = document.getElementById('userData').value;
+    console.log(userData, `userData`);
+
     const travelLenght = document.getElementById('travelLenght').value;
-    console.log (travelLenght);
+    console.log (travelLenght, `travelLenght`);
 
     const userAge = document.getElementById('userAge').value;
-    console.log (userAge);
-// ho i due valori dell'utente
-    const ticketPrice = (travelLenght * 0.21);
+    console.log (userAge, `userAge`);
+// ho i 3 valori dell'utente
 
+    const ticketPrice = (travelLenght * 0.21);
     console.log (ticketPrice);
 // ho il prezzo base
-   if (userAge <= 18) {
-   const youngPrice = (ticketPrice - (ticketPrice / 100 * 20)).toFixed(2) ;
 
+if (userAge == 'minorenne') {
+   const youngPrice = (ticketPrice - (ticketPrice / 100 * 20)).toFixed(2) ;
+// ho il prezzo giovani
 
    document.getElementById("answear").innerHTML = `Il tuo prezzo giovani é ${youngPrice}€`;
-
+// lo scrivo in pagina
    console.log(youngPrice);
 
-}else if (userAge >= 65){
+}else if (userAge == 'anziano'){
     const elderPrice = (ticketPrice - (ticketPrice /100 * 40)).toFixed(2);
-
+// ho il prezzo vecchi
 
     document.getElementById("answear").innerHTML = `Il tuo prezzo anziani é ${elderPrice}€`;
-
+// lo scrivo in pagina
     console.log(elderPrice);
 }
 
 else {
     document.getElementById("answear").innerHTML = `Il tuo prezzo normale é ${ticketPrice}€`;
 }
-
+// ho il prezzo gente normale e lo scrivo in pagina
 
 
 
